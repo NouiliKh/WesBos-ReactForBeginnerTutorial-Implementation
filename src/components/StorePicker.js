dab5ed24203ffc6813d68a8b390c0df0a1440d9f
NouiliKh <khalil.nouili@hotmail.com>
2018-03-18 20:04:04 +0100

    adding CSS file

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,7 +3,7 @@ import React from 'react'
 class StorePicker extends React.Component {
     render(){
         return (
-            <form className="Store-Selector">
+            <form className="store-selector">
             <h2>Please Pick A Store</h2>
             <input type ="text" required placeholder="store here"></input>
             <button type ="submit">Submit</button>
